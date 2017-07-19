a709bf792181aa9e039eafb2ae6339885be2b761
Garrett Haas <garrett.l.haas@gmail.com>
2017-07-19 16:45:14 -0500

    complete complete

--- src/main/java/org/launchcode/controllers/SearchController.java
+++ src/main/java/org/launchcode/controllers/SearchController.java
@@ -28,11 +28,17 @@ public class SearchController {
     @RequestMapping(value = "results", method = RequestMethod.GET)
     public String returnUserSearch(Model model,
         @RequestParam String searchType, @RequestParam String searchTerm) {
-        model.addAttribute("columns", ListController.columnChoices);
-        ArrayList<HashMap<String, String>> jobs = JobData.findByColumnAndValue(searchType, searchTerm);
-        model.addAttribute("jobs",jobs);
 
-        return "search";
+        if (searchType.equals("all")) {
+            ArrayList<HashMap<String, String>> jobs = JobData.findByValue(searchTerm);
+            model.addAttribute("jobs", jobs);
+            return "search";
+        }else {
+            model.addAttribute("columns", ListController.columnChoices);
+            ArrayList<HashMap<String, String>> jobs = JobData.findByColumnAndValue(searchType, searchTerm);
+            model.addAttribute("jobs", jobs);
+            return "search";
+        }
     }
 
 
